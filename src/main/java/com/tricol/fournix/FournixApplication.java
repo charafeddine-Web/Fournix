4660f350cd667479f3daf1e771f0cce5c00d1a8e
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-10-27 16:23:58 +0000

    Initial commit

---
+++ src/main/java/com/tricol/fournix/FournixApplication.java
@@ -0,0 +1,13 @@
+package com.tricol.fournix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FournixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FournixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tricol/fournix/FournixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tricol.fournix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FournixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
